d64418732d99cad894f7ddb6a26344c786bd37f5
Steven Santee <ssantee@fanatics.com>
2020-02-04 13:22:44 -0500

    add placeholder app icon

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+let iconImage = 'X.png';
+app.dock.setIcon( iconImage );
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
